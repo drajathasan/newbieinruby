8fd05eb9c1be58c86e39e35f511a826fc8dd7fe2
drajathasan <drajathasan20@gmail.com>
2018-12-10 17:42:51 +0700

    First commit

---
+++ dasar.rb
@@ -0,0 +1,19 @@
+### Dasar 1 Ruby
+### Variabel
+# a = 1
+# b = 2
+# puts a + b 
+
+### Dasar 2 Ruby
+### Array 
+# a = Array.new
+# a = ['drajat', 'hasan', 'makmur', 'alvisis']
+# puts a[0]
+
+### Dasar 3 Ruby
+### Associative Array 
+a = Hash.new
+a = [:drajat => "Hasan"]
+a = [:drajat => "Hasan"]
+
+puts a[0][:makmur]
